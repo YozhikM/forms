e70493d116481402daa8d259b1ede6cd340d71ab
Stas <botev.st@gmail.com>
2017-08-31 10:16:21 +0600

    Добавлено условие вызова

--- js/select.js
+++ js/select.js
@@ -1,4 +1,6 @@
-var select = document.querySelector('.select');
-var options = document.querySelector('.options');
-options.style.top = select.offsetTop + select.offsetHeight - 2 + 'px';
-options.style.width = select.offsetWidth + 'px';
+document.addEventListener("DOMContentLoaded", function() {
+  let select = document.querySelector(".select");
+  let options = document.querySelector(".options");
+  options.style.top = select.offsetTop + select.offsetHeight - 2 + "px";
+  options.style.width = select.offsetWidth + "px";
+});
